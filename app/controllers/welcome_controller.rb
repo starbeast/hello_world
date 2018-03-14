d8052078671962caa6c6e8431edcbf194c7318e0
Artsiom Kuts <bcdkey@gmail.com>
2018-03-14 21:56:37 +0300

    dump env

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     render json: {
       text: 'Hello World 4',
+      env: ENV.to_hash,
       ip: Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address)
     }
   end
